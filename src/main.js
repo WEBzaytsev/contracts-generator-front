123a522cb514d3edca77a17be33959ffe251838e
WEBzaytsev <studio@webzaytsev.ru>
2022-07-29 22:19:28 +0200

    add auto register components

--- src/main.js
+++ src/main.js
@@ -4,4 +4,22 @@ import upperFirst from 'lodash/upperFirst';
 import camelCase from 'lodash/camelCase';
 import './index.css';
 
-createApp(App).mount('#app');
+const requireComponent = require.context(
+    './components',
+    false,
+    /Base[A-Z]\w+\.(vue|js)$/
+);
+
+const app = createApp(App);
+
+requireComponent.keys().forEach((fileName) => {
+    const componentConfig = requireComponent(fileName);
+
+    const componentName = upperFirst(
+        camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+    );
+
+    app.component(componentName, componentConfig.default || componentConfig);
+});
+
+app.mount('#app');
