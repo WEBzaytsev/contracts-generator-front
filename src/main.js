4572d8526e3f1c9cc27cf019ae9e4b8de4ab51bd
WEBzaytsev <studio@webzaytsev.ru>
2022-07-29 22:19:03 +0200

    add lodash

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import upperFirst from 'lodash/upperFirst';
+import camelCase from 'lodash/camelCase';
 import './index.css';
 
 createApp(App).mount('#app');
