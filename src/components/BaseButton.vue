4c5195ee985a1c672c7331308a919d5ac345d0a9
WEBzaytsev <studio@webzaytsev.ru>
2022-07-29 22:20:02 +0200

    add button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <button v-bind="$attrs">{{ text }}</button>
+</template>
+
+<script>
+export default {
+    name: 'BaseButton',
+    props: {
+        text: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style scoped></style>
