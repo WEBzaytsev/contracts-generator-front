82e8ea4972f83d0b6374c85ebf3985c3b5a1df13
WEBzaytsev <studio@webzaytsev.ru>
2022-07-30 14:25:59 +0200

    add some styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ['./index.html', './src/**/*.{vue,js}'],
     theme: {
-        extend: {},
+        extend: {
+            spacing: {
+                7.5: '1.875rem',
+            },
+        },
     },
     plugins: [],
 };
