c154f5899b0cc0f4085e2d2bb1ccee857e2b2565
WEBzaytsev <studio@webzaytsev.ru>
2022-07-31 23:44:18 +0200

    create data-group component

---
+++ src/components/FormFieldsGroup.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <fieldset>
+            <BaseFieldsetLegend :text="title" />
+            <slot></slot>
+        </fieldset>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FormFieldsGroup',
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style scoped></style>
