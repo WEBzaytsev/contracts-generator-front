b656a1ce1826c3026c0d72d3eb55b9610fa37d4b
WEBzaytsev <studio@webzaytsev.ru>
2022-08-01 14:32:42 +0200

    add datepicker

--- src/main.js
+++ src/main.js
@@ -2,15 +2,18 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import upperFirst from 'lodash/upperFirst';
 import camelCase from 'lodash/camelCase';
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
 import './index.css';
 
+const app = createApp(App);
+
+// register "Base" components
 const requireComponent = require.context(
     './components',
     false,
     /Base[A-Z]\w+\.(vue|js)$/
 );
-
-const app = createApp(App);
 
 requireComponent.keys().forEach((fileName) => {
     const componentConfig = requireComponent(fileName);
@@ -21,5 +24,8 @@ requireComponent.keys().forEach((fileName) => {
 
     app.component(componentName, componentConfig.default || componentConfig);
 });
+
+// add Datepicker
+app.component('VueDatepicker', Datepicker);
 
 app.mount('#app');
