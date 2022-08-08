bed8cc724c7b134975ab87da19af8debe0130312
WEBzaytsev <studio@webzaytsev.ru>
2022-08-08 19:05:32 +0200

    some styling button

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,5 +1,10 @@
 <template>
-    <button v-bind="$attrs">{{ text }}</button>
+    <button
+        class="px-4 py-2 rounded-lg shadow-lg border-black/10 border border-solid font-bold transition-all duration-300 hover:bg-stone-200"
+        v-bind="$attrs"
+    >
+        {{ text }}
+    </button>
 </template>
 
 <script>
